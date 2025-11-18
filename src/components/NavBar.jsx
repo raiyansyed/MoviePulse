7de0f5aa81eba61ff91bb4d09cc9bf2caf5e3938
Raiyan <raiyansyed79@gmail.com>
2025-11-18 14:10:26 +0530

    rename

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="font-bold text-2xl bg-[#403963] w-full h-20 flex items-center px-6">
       <Link to="/" className="shrink-0">
-        Movie Info
+        Movie Pulse
       </Link>
       <div className="ml-auto flex items-center gap-8 mr-4">
         <Link to="/">Home</Link>
